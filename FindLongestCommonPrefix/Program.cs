4a4b71f9545580cadd7d8279aa33382e9733a2d3
Sharp336 <39415926+Sharp336@users.noreply.github.com>
2022-10-19 12:48:25 +0300

    Initial commit

---
+++ FindLongestCommonPrefix/Program.cs
@@ -0,0 +1,21 @@
+﻿public class Program
+{
+    static string FLCP(string[] strs) {
+        var prefix = strs[0];
+        foreach (var word in strs)
+        {
+            while (word.IndexOf(prefix, StringComparison.Ordinal) != 0)
+            {
+                prefix = prefix.Substring(0, prefix.Length - 1);
+                if (prefix == "") return "Completely different words";
+            }
+        }
+        return prefix;
+    }
+	
+    public static void Main()
+    {
+        Console.WriteLine($"Answer = {FLCP(new[] {"flower", "flow", "flight"})}"); 
+        Console.WriteLine($"Answer = {FLCP(new[] {"dog", "racecar", "car"})}"); 
+    }
+}
